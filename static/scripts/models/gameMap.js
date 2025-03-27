f0fe8cee2adc5c94e7079936fdb539cd23b38134
Mloan <loan.mata@ynov.com>
2025-03-27 15:15:07 +0100

    Feat(gameMap): setting obstacles list as a private attributes. Add a private ready attributs and some getter/setters

--- static/scripts/models/gameMap.js
+++ static/scripts/models/gameMap.js
@@ -1,5 +1,9 @@
 
 class GameMap {
+    // private attributs
+    #obstacles = []
+    #ready = false
+
     constructor(mapNumber=1) {
         fetch(`/static/assets/map/map${mapNumber}.json`)
         .then(res => res.json())
@@ -10,13 +14,12 @@ class GameMap {
         .catch(err => {
             throw new Error("Unable to fetch map data. " + err, {cause: err})
         })
-        this.obstacles = []
-        
     }
 
+    // private methods
     #load() {
         for(let el of this.mapJson.grid) {
-            this.obstacles.push(
+            this.#obstacles.push(
                 {
                     x: el.x * this.mapJson.config.gridXSize,
                     y: el.y * this.mapJson.config.gridYSize,
@@ -26,6 +29,17 @@ class GameMap {
                 }
             )
         }
+        this.#ready = true;
     }
+
+    getObstacles() { return this.#obstacles}
+
+    updateObstaclesAt(obstacles, index) {
+        if(index < 0) throw new Error("Index out of bounds")
+        if(index > this.#obstacles.length) throw new Error("Index out of bounds")
+        this.#obstacles[index] = obstacles
+    }
+
+    isReady() { return this.#ready }
 }
 
