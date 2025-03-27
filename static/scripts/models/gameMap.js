dd0ea3557dd26ed9cf63d530048be04e38b47066
Mloan <loan.mata@ynov.com>
2025-03-27 15:27:14 +0100

    docs(JSDocs): add JSDocs on gameMap.js

--- static/scripts/models/gameMap.js
+++ static/scripts/models/gameMap.js
@@ -1,45 +1,76 @@
+import Obstacles from "./obstacle";
 
 class GameMap {
-    // private attributs
-    #obstacles = []
-    #ready = false
+  // private attributs
+  /**
+   * mapJson - The json object that contains the map data
+   * @type {Object}
+   */
+  #mapJson = {};
+  /**
+   * The list that contains all obstacles
+   * @type {Obstacles[]}
+   */
+  #obstacles = [];
+  /**
+   * ready - A flag that indicates if the map is fully loaded and ready
+   * @type {boolean}
+   */
+  #ready = false;
 
-    constructor(mapNumber=1) {
-        fetch(`/static/assets/map/map${mapNumber}.json`)
-        .then(res => res.json())
-        .then(json => {
-            this.mapJson = json
-            this.#load()
-        })
-        .catch(err => {
-            throw new Error("Unable to fetch map data. " + err, {cause: err})
-        })
+  constructor(mapNumber = 1) {
+    fetch(`/static/assets/map/map${mapNumber}.json`)
+      .then((res) => res.json())
+      .then((json) => {
+        this.#mapJson = json;
+        this.#load();
+      })
+      .catch((err) => {
+        throw new Error("Unable to fetch map data. " + err, { cause: err });
+      });
+  }
+
+  // private methods
+  #load() {
+    let config = this.#mapJson.config
+    for (let el of this.#mapJson.grid) {
+      this.#obstacles.push({
+        x: el.x * config.gridXSize,
+        y: el.y * gridYSize,
+        onCollide: document[el.onCollide],
+        canGoThrough: el.canGoThrough,
+        img: config.imageSet[el.img],
+      });
     }
+    this.#ready = true;
+  }
 
-    // private methods
-    #load() {
-        for(let el of this.mapJson.grid) {
-            this.#obstacles.push(
-                {
-                    x: el.x * this.mapJson.config.gridXSize,
-                    y: el.y * this.mapJson.config.gridYSize,
-                    onCollide: document[el.onCollide],
-                    canGoThrough: el.canGoThrough,
-                    img: this.mapJson.config.imageSet[el.img]
-                }
-            )
-        }
-        this.#ready = true;
-    }
+  /**
+   * Return the list of obstacles
+   * @returns {Obstacles[]} 
+   */
+  getObstacles() {
+    return this.#obstacles;
+  }
 
-    getObstacles() { return this.#obstacles}
+  /**
+   * Re-set the obstacle at the given index
+   * @param {Obstacles} obstacles - The obstacle to set
+   * @param {Number} index - The index of the obstacle to set
+   */
+  updateObstaclesAt(obstacle, index) {
+    if (index < 0) throw new Error("Index out of bounds");
+    if (index > this.#obstacles.length) throw new Error("Index out of bounds");
+    this.#obstacles[index] = obstacle;
+  }
 
-    updateObstaclesAt(obstacles, index) {
-        if(index < 0) throw new Error("Index out of bounds")
-        if(index > this.#obstacles.length) throw new Error("Index out of bounds")
-        this.#obstacles[index] = obstacles
-    }
-
-    isReady() { return this.#ready }
+  /**
+   * Return true if the map is fully loaded and ready
+   * @returns {Boolean}
+   */
+  isReady() {
+    return this.#ready;
+  }
 }
 
+export default GameMap;
---
+++ static/scripts/models/obstacle.js
@@ -0,0 +1,5 @@
+class Obstacles {
+
+}
+
+export default Obstacles;
