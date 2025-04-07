6e16fb8f12a4b988a10d6b96d363746e3508ab5d
DantesDels <delver.sebastien@gmail.com>
2025-04-07 16:39:09 +0200

    feature(GUI) : Adding Effects to ArrowsNavMap

---
+++ static/scripts/GUI/arrowsInputHoverEffect.js
@@ -0,0 +1,45 @@
+import MovementController  from '../controllers/move.js';
+
+function arrowsInputEffect() {
+
+    document.addEventListener('keydown', (event) => {
+        console.log(`Key pressed: ${event.key}`);
+    
+    switch (event.key) {
+      case 'ArrowUp':
+        document.querySelector('.fa-arrow-up').parentElement.classList.add('active');
+        break;
+      case 'ArrowDown':
+        document.querySelector('.fa-arrow-down').parentElement.classList.add('active');
+        break;
+      case 'ArrowLeft':
+        document.querySelector('.fa-arrow-left').parentElement.classList.add('active');
+        break;
+      case 'ArrowRight':
+        document.querySelector('.fa-arrow-right').parentElement.classList.add('active');
+        break;
+    }
+  });
+
+  document.addEventListener('keyup', (event) => {
+
+    switch (event.key) {
+      case 'ArrowUp':
+        document.querySelector('.fa-arrow-up').parentElement.classList.remove('active');
+        break;
+      case 'ArrowDown':
+        document.querySelector('.fa-arrow-down').parentElement.classList.remove('active');
+        break;
+      case 'ArrowLeft':
+        document.querySelector('.fa-arrow-left').parentElement.classList.remove('active');
+        break;
+      case 'ArrowRight':
+        document.querySelector('.fa-arrow-right').parentElement.classList.remove('active');
+        break;
+    }
+
+  });
+  
+}
+
+arrowsInputEffect()
