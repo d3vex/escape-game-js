3cc81e9905eaea2d1792ff8d530ee93db5de0fbc
DantesDels <delver.sebastien@gmail.com>
2025-04-07 20:14:52 +0200

    feature(GUI) :
    Add countdown timer functionality to the GUI
    Add Basic ATH version
    
    - Implemented a startTimer function that initializes a countdown timer.
    - The timer displays remaining time in MM:SS format and updates every second.
    - Added a message display when the timer reaches zero.
    - Initialized the timer for a duration of 15 minutes in the GUI.
    - Added enigmaBox, questBox (checkbox for Goal to achieve the enigma completion), interactBox,

---
+++ static/scripts/GUI/startTimer.js
@@ -0,0 +1,28 @@
+function startTimer(durationInMinutes, displayElement) {
+    
+    let remainingTime = durationInMinutes * 60; // Seconds to Minutes conversion
+
+    function updateTimer() {
+        const minutes = Math.floor(remainingTime / 60);
+        const seconds = remainingTime % 60;
+
+        // Time Format (ex: 14:59)
+        displayElement.textContent = 
+        `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+        if (remainingTime > 0) {
+            remainingTime--;
+        } else {
+            clearInterval(timerInterval); //Stop when arrived at 0
+            displayElement.textContent = "Time's up!";
+        }
+    }
+
+    // Update timer every seconds
+    const timerInterval = setInterval(updateTimer, 1000);
+    updateTimer();
+}
+
+// Timer initialised
+const timerElement = document.querySelector('.timer');
+startTimer(15, timerElement); // 15 minutes
