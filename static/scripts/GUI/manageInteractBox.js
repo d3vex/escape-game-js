991b7b36e4c7212d483a85e75a85f8a0ffa6a1f3
DantesDels <delver.sebastien@gmail.com>
2025-04-08 18:15:06 +0200

    feat(GUI):
    add interaction functions and scripts for interactBox visibility

---
+++ static/scripts/GUI/manageInteractBox.js
@@ -0,0 +1,7 @@
+function hideInteractBox() {
+    document.querySelector(".interactBox").style.bottom = "calc(-10vh - 4px)"
+}
+
+function revealInteractBox() {
+    document.querySelector(".interactBox").style.bottom = "calc(0.1rem)"
+}
