718effcb131890193c3b0a52d78fe5397ee0c7d3
Mloan <loan.mata@ynov.com>
2025-04-08 14:57:12 +0200

    Feat(enigme1): adding a method to take the key, and another to open the door

--- static/scripts/enigmes.js/enigme1.js
+++ static/scripts/enigmes.js/enigme1.js
@@ -1,16 +1,73 @@
+import LocalStorageService from "../services/localStorageService.js";
 
 class Enigme1 {
     
-
-    static get Enigme() {
+    /**
+     * This method allow to get the Enigme1 object
+     * It will return an object with the following properties:
+     * - id: the id of the enigme
+     * - name: the name of the enigme
+     * - description: the description of the enigme
+     * - hint: the hint of the enigme
+     * - hintPrice: the price of the hint
+     * - level: the level of the enigme
+     * 
+     * @returns {Object}
+     */
+    static get enigme() {
         return {
             id: 1,
             name: "Find the key",
             description: "You are locked in a room and you need to find the key to escape.",
             hint: "Search behind the pellows.",
+            hintPrice: 50,
             level: 1,
         };
     }
+    
+    /**
+     * This method allow to get the first key.
+     * @returns {{
+     *      success: Boolean,
+     *      message: String}
+     * }
+     * 
+     */
+    static takeKey() {
+        let keyIsAvailableToTake = LocalStorageService.getUserAttributes("enigme1.Key")==false?true:false;
+        if (keyIsAvailableToTake) {
+            LocalStorageService.setUserAttributes("enigme1.Key", true);
+            return {
+                success: true,
+                message: "You took the key.",
+            };
+        } else {
+            return {
+                success: false,
+                message: "You already took the key.",
+            };
+        }
+    }
 
+    static openDoor() {
+        let keyIsAvailableToTake = LocalStorageService.getUserAttributes("enigme1.Key")==false?true:false;
+        if (keyIsAvailableToTake) {
+            return {
+                success: false,
+                message: "You need to find the key first.",
+            };
+        } else {
+            LocalStorageService.setUserAttributes("enigme1.Door", true);
+            LocalStorageService.setUserAttributes("enigme1.timestampWhenEnded", Date.now());
+            LocalStorageService.setUserAttributes("enigme1.isFinished", true);
+            
+            // This is actually unavailable but will be used to open the door and unlock the next room
+            //renderer.nextFrame();
 
+            return {
+                success: true,
+                message: "You opened the door.",
+            };
+        }
+    }
 }
