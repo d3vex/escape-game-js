12b25fe6037461865f1b2a9e245d0ee64374e468
Mloan <loan.mata@ynov.com>
2025-04-08 14:31:16 +0200

    Feat(enigme1): adding a getter to obtain all details about the first enigme

---
+++ static/scripts/enigmes.js/enigme1.js
@@ -0,0 +1,16 @@
+
+class Enigme1 {
+    
+
+    static get Enigme() {
+        return {
+            id: 1,
+            name: "Find the key",
+            description: "You are locked in a room and you need to find the key to escape.",
+            hint: "Search behind the pellows.",
+            level: 1,
+        };
+    }
+
+
+}
