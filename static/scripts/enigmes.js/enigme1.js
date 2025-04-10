727b34f2f433063772e2d5197d44bb7cd65edbd6
Mloan <loan.mata@ynov.com>
2025-04-10 18:35:19 +0200

    Feat(enigme1): update openDoor method to be asynchronous and integrate game state transition

--- static/scripts/enigmes.js/enigme1.js
+++ static/scripts/enigmes.js/enigme1.js
@@ -1,5 +1,5 @@
 import LocalStorageService from "../services/localStorageService.js";
-
+import Game from "../game.js";
 const maxTimer = 15 * 60 * 1000; // 15 minutes in milliseconds
 
 class Enigme1 {
@@ -78,7 +78,7 @@ class Enigme1 {
    *      message: String}
    * }
    */
-  static openDoor() {
+  static async openDoor() {
     let keyIsAvailableToTake =
       LocalStorageService.getUserAttributes("enigme1.Key") == false
         ? true
@@ -96,8 +96,7 @@ class Enigme1 {
       );
       LocalStorageService.setUserAttributes("enigme1.isFinished", true);
 
-      // This is actually unavailable but will be used to open the door and unlock the next room
-      //renderer.nextFrame();
+      await Game.getInstance().nextState()
 
       return {
         success: true,
