f1791873eca309cb5dfb78088cf650b8aa9e2bb9
DantesDels <delver.sebastien@gmail.com>
2025-04-10 14:24:46 +0200

    feature(GUI):
    functions imported correctly,
    mainMessage & actionRealisedMessage can be changed.

--- static/scripts/GUI/manageInteractBox.js
+++ static/scripts/GUI/manageInteractBox.js
@@ -1,7 +1,24 @@
 function hideInteractBox() {
-    document.querySelector(".interactBox").style.bottom = "calc(-10vh - 4px)"
+  document.querySelector(".interactBox").style.bottom = "calc(-10vh - 4px)";
 }
 
-function revealInteractBox() {
-    document.querySelector(".interactBox").style.bottom = "calc(0.1rem)"
+function revealInteractBox(mainMessage, actionRealisedMessage) {
+    
+  const mainMessageElement = 
+    document.querySelector(".interactBox p");
+
+  const actionRealisedMessageElement =
+    document.querySelector(".interactBox span");
+
+  if (mainMessage) {
+    mainMessageElement.firstChild.textContent = mainMessage;
+  }
+
+  if (actionRealisedMessage) {
+    actionRealisedMessageElement.firstChild.textContent = actionRealisedMessage;
+  }
+
+  document.querySelector(".interactBox").style.bottom = "0.1rem";
 }
+
+export default { hideInteractBox, revealInteractBox };
