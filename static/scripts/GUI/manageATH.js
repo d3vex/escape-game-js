f4890e7757cb77f424563bfbf3d48c2f559b3e75
DantesDels <delver.sebastien@gmail.com>
2025-04-10 16:46:34 +0200

    feat(GUI):
    add wrapperElement in manageATH()

--- static/scripts/GUI/manageATH.js
+++ static/scripts/GUI/manageATH.js
@@ -1,28 +1,38 @@
 function hideATH() {
   document.addEventListener("keydown", (event) => {
     if (event.key === "h") {
+      const wrapperElement = document.querySelector(".wrapper");
       const timerElement = document.querySelector(".timer");
       const enigmaBoxElement = document.querySelector(".enigmaBox");
       const questBoxElement = document.querySelector(".questBox");
 
       if (timerElement.style.transform === "scale(0.5)") {
-        revealATH(timerElement, enigmaBoxElement, questBoxElement);
+        revealATH(wrapperElement, timerElement, enigmaBoxElement, questBoxElement);
+
       } else {
+        wrapperElement.style.transform = "scale(0.5)";
+        wrapperElement.style.opacity = "0.5";
+        wrapperElement.style.transition = "transform 1s ease, opacity 0.5s ease";
+
         timerElement.style.transform = "scale(0.5)";
         timerElement.style.opacity = "0.5";
-        timerElement.style.transition = "transform 1s ease, opacity 1s ease";
+        timerElement.style.transition = "transform 1s ease, opacity 0.5s ease";
 
         enigmaBoxElement.style.left = "calc(-21vw - 4px)";
-        enigmaBoxElement.style.transition = "left 1s ease";
+        enigmaBoxElement.style.transition = "left 0.5s ease";
 
         questBoxElement.style.right = "calc(-26vw - 4px)";
-        questBoxElement.style.transition = "right 1s ease";
+        questBoxElement.style.transition = "right 0.5s ease";
       }
     }
   });
 }
 
-function revealATH(timerElement, enigmaBoxElement, questBoxElement) {
+function revealATH(wrapperElement, timerElement, enigmaBoxElement, questBoxElement) {
+  wrapperElement.style.transform = "scale(1)";
+  wrapperElement.style.opacity = "1";
+  wrapperElement.style.transition = "transform 1s ease, opacity 1s ease";
+
   timerElement.style.transform = "scale(1)";
   timerElement.style.opacity = "1";
   timerElement.style.transition = "transform 1s ease, opacity 1s ease";
