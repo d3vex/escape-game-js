8524caf0afafdb6046cdb8f970bf878f5cba6c22
Mloan <loan.mata@ynov.com>
2025-04-16 17:57:18 +0200

    Feat(enigmeBox): implement 3 new method to edit the enigme box content

---
+++ static/scripts/GUI/enigmeManager.js
@@ -0,0 +1,35 @@
+/**
+ * This method allow to modify the title of the enigme box located at the left of the screen
+ * 
+ * @param {String} title 
+ */
+function updateEnigmeBoxTitle(title) {  
+    let enigmeBox = document.querySelector(".enigmaBox")
+    if(!enigmeBox) return
+    enigmeBox.querySelector("h4")?.innerText = title;
+}
+
+/**
+ * This method allow to modify the description of the enigme in the enigme box located at the left of the screen
+ * 
+ * @param {String} description 
+ */
+function updateEnigmeBoxDescription(description) {  
+    let contentBox = document.querySelector(".enigmaBox .contentEnigmaBox")
+    if(!contentBox) return
+    enigmeBox.querySelector("span")?.innerText = description;
+}
+
+/**
+ * Updates the content of the enigme box located at the left of the screen by modifying its title and description.
+ * 
+ * @param {String} title - The new title to set for the enigme box.
+ * @param {String} description - The new description to set for the enigme box.
+ */
+function updateEnigmeBoxContent(title, description) {
+    updateEnigmeBoxTitle(title),
+    updateEnigmeBoxDescription(description)
+}
+
+
+export { updateEnigmeBoxContent, updateEnigmeBoxTitle, updateEnigmeBoxDescription}
